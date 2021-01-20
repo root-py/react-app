29ead2495710b9dd53d16ab969b400bbcd28eb4a
sujit <root@gmail.com>
2021-01-20 05:23:18 -0800

    Created Card.jsx file

---
+++ src/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+
+function Card(props){
+    return(
+        <div>
+            <h2>{props.title}</h2>
+            <h2>{props.sname}</h2>
+
+        </div>
+    );
+    }
+export default Card;
