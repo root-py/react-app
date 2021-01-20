39c2d4ed0fb133234906ebc7fe6d747a6140aaad
sujit <root@gmail.com>
2021-01-20 05:21:40 -0800

    Initial webpage created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from './Card'
+
+
+
+
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <>
+        <h1>Hi this is hello world program of my first React app</h1>,
+        <Card 
+            title="A Dark series of the Netflix" 
+            sname="Dark">
+
+                    
+                
+        </Card>
+
+    
+    </>,
+    document.getElementById("root")
+);
